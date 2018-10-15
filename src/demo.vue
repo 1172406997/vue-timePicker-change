122c73c1a7b5b67e0e08f429b415e496930bbb05
star <1172406997@qq.com>
2018-10-15 18:11:22 +0800

    插件修改更新

---
+++ src/demo.vue
@@ -0,0 +1,18 @@
+<style scoped>
+
+</style>
+
+<template>
+    <datepicker ></datepicker>
+    <!--<datepicker format="YYYY-M-D" value="2015-9-5"></datepicker>
+    <datepicker :readonly="true" format="MMM/D/YYYY" width="300px" name="date"></datepicker>-->
+</template>
+
+<script>
+import datepicker from './datepicker.vue';
+
+export default {
+    components: { datepicker }
+};
+</script>
+
